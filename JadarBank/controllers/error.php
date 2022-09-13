9937d5bc9104b36553819282061b5a260b7b39ea
JesusHT <102433533+JesusHT@users.noreply.github.com>
2022-09-12 21:18:41 -0500

    Nuevo Repositorio Jadar Bank

---
+++ JadarBank/controllers/error.php
@@ -0,0 +1,9 @@
+<?php
+
+    class Errorr {
+        function __construct(){
+            echo "<p>Error al cargar el recurso</p>";
+        }
+    }
+
+?>
---
+++ JadarBank/controllers/main.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Main {
+        function __construct(){
+            echo "<p>Nuevo controlador</p>";
+        }
+
+        function saludo(){
+            echo '<p>Ejecutaste el metodo saludo</p>';
+        }
+    }
+
+?>
