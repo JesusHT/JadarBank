d1943a580ba7c96373a47f80ab2dea0f493f4323
JesusHT <jesus43.gts@gmail.com>
2022-11-15 01:15:57 -0600

    Added view client and lib qrcode

--- views/main/index.php
+++ views/main/index.php
@@ -5,15 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="<?php echo constant('URL'); ?>public/img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet"    href="<?php echo constant('URL'); ?>public/css/global.css">
+    <link rel="stylesheet"    href="<?php echo constant('URL'); ?>public/css/cliente.css">
     <title>JADAR BANK</title>
 </head>
 <body>
     <?php $this -> showMessages();?>
     <main class="main">
         <?php $this -> navController(); ?>
-        <section class="content"  id="content-main">
-            <h1 class="center">Hola <?php echo $cliente['name'];?>!</h1>
+        <section class="content" id="content-main">
+            <!-- <div class="massage bg-warning"><p class="">Tiene un prestamo que esta por vencerse</p></div> -->
+            <div class="header">
+                <div class="content-customer-data">
+                    <img src="<?php echo constant('URL') . 'public/img/' . $cliente['img_client']; ?>" alt="Imagen del cliente">
+                    <h1>Bienvenido <?php echo $cliente['name']; ?></h1>
+                </div>
+            </div>
+            <div class="body">
+                <p>Saldo actual: mxn</p>
+            </div>
         </section>
     </main>  
 </body>
